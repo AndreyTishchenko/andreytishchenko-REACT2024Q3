c6c0b7dcdceecbfe8783934cc047a4fbf6e4f59a
AndreyTishchenko <Tishchenko.Andrew@yandex.ru>
2024-07-14 20:37:45 +0700

    fix: commit change which i forgot to safe

--- StarWarsDatabase/src/components/error/error.tsx
+++ StarWarsDatabase/src/components/error/error.tsx
@@ -11,13 +11,11 @@ export default class ErrorBoundary extends React.Component<
     }
 
     static getDerivedStateFromError() {
-        // Update state so the next render will show the fallback UI.
         return { hasError: true }
     }
 
     render() {
         if (this.state.hasError) {
-            // You can render any custom fallback UI
             return (
                 <>
                     <div className="errorDiv">
