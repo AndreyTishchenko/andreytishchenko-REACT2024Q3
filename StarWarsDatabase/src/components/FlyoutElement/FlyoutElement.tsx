929de7e794b9f7e7e38281c1362289bddd8703e9
AndrewTishchenko <andy.tishchenko@gmail.com>
2025-02-24 01:42:48 +0700

    feat: add download button to flyout element

--- StarWarsDatabase/src/components/FlyoutElement/FlyoutElement.tsx
+++ StarWarsDatabase/src/components/FlyoutElement/FlyoutElement.tsx
@@ -14,10 +14,28 @@ export default function FlyoutElement() {
         })
     }
 
+    function downloadAllInCSV() {
+        const csvContent =
+            'data:text/csv;charset=utf-8,' +
+            planets
+                .map((planet) => {
+                    return `PlanetName: ${planet.name},\nDiameter: ${planet.diameter},\nRotation Period: ${planet.rotation_period},\norbital_period: ${planet.orbital_period},\nGravity: ${planet.gravity},\nPopulation: ${planet.population},\nClimate: ${planet.climate},\nterrain: ${planet.terrain},\nsurface_water: ${planet.surface_water},\nresidents: ${planet.residents},\nfilms: ${planet.films},\nurl: ${planet.url},\nCreated: ${planet.created},\nEdited: ${planet.edited}\n\n`
+                })
+                .join('\n')
+        const encodedUri = encodeURI(csvContent)
+        const link = document.createElement('a')
+        link.setAttribute('href', encodedUri)
+        link.setAttribute('download', `${planets.length}planets.csv`)
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
+
     return (
         <div>
             <h3>Choosen Elements: {planets.length}</h3>
             <button onClick={deleteAll}>Unselect All</button>
+            <button onClick={downloadAllInCSV}>Download All</button>
         </div>
     )
 }
